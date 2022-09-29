3a95e040101406ff9de26938c0cc852ab0189fb1
paulina-11 <paulinavila.lopez@gmail.com>
2022-09-28 21:34:31 -0500

    ejemplo con ejercicios

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@
 // req.body ---> Recibimos los datos que llegan en el body
 
 //  Controladores de Mascotas
-// POST
+// POST por body
 const crearMascota = (req, res) => {
   console.log("crear mascota");
   const nombre = req.body.nombre
@@ -32,6 +32,7 @@ const obtenerTodasMascotas = (req, res) => {
   })
 };
 
+
 const obtenerMascota = (req, res) => {
   console.log("obtener una mascota");
 };
@@ -44,10 +45,53 @@ const eliminarMascota = (req, res) => {
   console.log("eleminar mascota");
 };
 
+
+
+// Ejercicios
+// Realiza los siguientes end points y testealos con Postman, insomnia, Thunder, etc...
+// Agrega un endpoint '/api/' que responda a una petición de tipo GET con un código de estado 200 y el siguiente json: { 'mensaje':'hola mundo' }
+// Agrega un endpoint '/api/suma' que responda a una petición de tipo GET con la suma de dos números que reciba mediante las querys num1 y num2. El servidor debe responder con un código de estado 200 y un json como el siguiente: { 'resultado': 7 }
+
+// Agrega un endpoint '/api/usuario/' que responda a una petición de tipo GET con el nombre que sea recibido a través de params. El servidor debe responder con un código de estado 200 y un json como este: { 'usuario': 'Edwin' }
+// Agrega un endpoint '/api/body que responda a una petición de tipo PUT con el body que el cliente envíe al hacer la petición. Ejemplo: cliente envía un body desde postman o insomnia que luce como este:
+// { “nombre”: “Maui”, “ocupacion”: “Sensei” }
+// Entonces, el servidor debe responder con un objeto idéntico al que envía el cliente, junto con un status de respuesta 200.
+//  POST por query
+const apiController = (req,res)=>{
+    res.status(200).send({
+        "mensaje":"Hola mundo"
+    })
+}
+// get query
+const apiSumaController = (req, res) => {
+    const num1 = Number(req.query.num1)
+    const num2 =  Number(req.query.num2)
+   const resSuma = num1 + num2
+
+   res.status(200).send({
+    resultado : resSuma
+   })
+};
+
+
+const apiUsuarioController=(req,res)=>{
+console.log(req.params);
+ res.status(200).send({
+   usuario: req.params.nombre
+ });
+}
+const apiBodyController = (req,res)=>{
+    const data = req.body
+    res.status(200).send(data);
+}
 module.exports = {
   crearMascota,
   obtenerTodasMascotas,
   obtenerMascota,
   modificarMascota,
   eliminarMascota,
+  apiController,
+  apiSumaController,
+  apiUsuarioController,
+  apiBodyController
 };
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ const {
   obtenerMascota,
   modificarMascota,
   eliminarMascota,
+
+  apiController,
+  apiSumaController,
+  apiUsuarioController,
+  apiBodyController
 } = require("../controllers");
 
 // CRUD de mascotas
@@ -14,5 +19,13 @@ router.get("/mascotas", obtenerTodasMascotas);
 router.get("/mascotas/:idMascota", obtenerMascota);
 router.put("/mascotas/:idMascota", modificarMascota);
 router.delete("/mascotas/:idMascota", eliminarMascota);
+
+
+router.get("/api", apiController);
+// si son query no necesita : 
+router.get("/api/suma", apiSumaController);
+// por parametro es con /:
+router.get("/api/usuario/:nombre", apiUsuarioController);
+router.put("/api/body", apiBodyController);
 
 module.exports = router;
